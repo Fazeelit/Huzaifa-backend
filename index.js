da07df56334c04a85a712e2e6e9a632043e7993c
Fazeel Ahmad <108638723+Fazeelit@users.noreply.github.com>
2026-06-04 21:58:27 +0500

    ok

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ const defaultAllowedOrigins = [
   "http://127.0.0.1:3000",
   "http://127.0.0.1:3001",
   "http://localhost:5173",
-  "https://huzaifa-feroza.vercel.app/",
-  "https://huzaifa-autos.vercel.app",
+  "https://huzaifa-autoz.vercel.app/",
 ];
 
 const allowedOrigins = new Set(
